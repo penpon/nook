9e8fb2a29cbcac27018213f6a251ba90274c56a0
penpon <penpon>
2025-12-11 04:24:59 +0900

    fix(feed_utils): re-apply explicit import with noqa to prevent linter removal

--- nook/common/feed_utils.py
+++ nook/common/feed_utils.py
@@ -1,3 +1,4 @@
 """互換性レイヤー: nook.services.base.feed_utils への転送"""
 
 from nook.services.base.feed_utils import *  # noqa: F403, F401
+from nook.services.base.feed_utils import _get_entry_value  # noqa: F401
